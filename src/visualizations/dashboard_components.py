f8b457a75fe60f95b307563215d46b9ee535d275
Ethan Miller <siporlicecur@gmx.net>
2023-01-05 14:59:01 -0500

    build: Set up Dockerfile for dashboard deployment

--- src/visualizations/dashboard_components.py
+++ src/visualizations/dashboard_components.py
@@ -27,3 +27,5 @@ def create_individual_explanation_waterfall_chart(explanation_values, title="Ind
     return fig
 
 # Change on 2023-01-04 16:57:10: feat: Add support for new XAI methods (e.g., Grad-CAM)
+
+# Change on 2023-01-05 14:59:01: build: Set up Dockerfile for dashboard deployment
