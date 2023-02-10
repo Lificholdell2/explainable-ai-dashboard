f02d6ef105b3e3aeb006edb0cebbf2212d230b01
Ethan Miller <siporlicecur@gmx.net>
2023-02-10 09:04:49 -0500

    refactor: Improve dashboard layout and responsiveness

--- app.py
+++ app.py
@@ -98,3 +98,5 @@ if __name__ == '__main__':
 # Change on 2023-02-06 12:28:16: test: Add unit tests for XAI explanation generation
 
 # Change on 2023-02-07 09:17:17: feat: Add support for new XAI methods (e.g., Grad-CAM)
+
+# Change on 2023-02-10 09:04:49: refactor: Improve dashboard layout and responsiveness
