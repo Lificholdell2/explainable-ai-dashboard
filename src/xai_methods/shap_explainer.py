861e2221795dd8c4629cd4dc517fca5b6bac4c08
Ethan Miller <siporlicecur@gmx.net>
2023-02-15 12:27:16 -0500

    fix: Correct data loading and preprocessing for XAI models

--- src/xai_methods/shap_explainer.py
+++ src/xai_methods/shap_explainer.py
@@ -23,3 +23,5 @@ class ShapExplainer:
 # Change on 2023-01-24 14:54:24: refactor: Modularize XAI method implementations
 
 # Change on 2023-02-02 16:00:42: fix: Resolve issues with interactive graph updates
+
+# Change on 2023-02-15 12:27:16: fix: Correct data loading and preprocessing for XAI models
