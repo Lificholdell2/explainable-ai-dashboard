185c3d17e5f3c4a884070e95f159e30e6ca1f814
Ethan Miller <siporlicecur@gmx.net>
2023-02-20 13:23:46 -0500

    chore: Upgrade Dash and Plotly dependencies

--- src/visualizations/dashboard_components.py
+++ src/visualizations/dashboard_components.py
@@ -47,3 +47,5 @@ def create_individual_explanation_waterfall_chart(explanation_values, title="Ind
 # Change on 2023-02-13 12:27:19: chore: Clean up unused visualization assets
 
 # Change on 2023-02-13 09:17:04: docs: Create detailed API documentation for XAI components
+
+# Change on 2023-02-20 13:23:46: chore: Upgrade Dash and Plotly dependencies
