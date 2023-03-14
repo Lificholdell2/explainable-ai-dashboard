e037c28cb93e75122b93f6486690f2de7258cf54
Ethan Miller <siporlicecur@gmx.net>
2023-03-14 13:33:50 -0400

    docs: Create detailed API documentation for XAI components

--- src/visualizations/dashboard_components.py
+++ src/visualizations/dashboard_components.py
@@ -57,3 +57,5 @@ def create_individual_explanation_waterfall_chart(explanation_values, title="Ind
 # Change on 2023-03-09 16:02:44: chore: Clean up unused visualization assets
 
 # Change on 2023-03-14 14:09:19: style: Apply consistent styling to dashboard components
+
+# Change on 2023-03-14 13:33:50: docs: Create detailed API documentation for XAI components
