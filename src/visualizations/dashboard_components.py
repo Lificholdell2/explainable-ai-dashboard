3fa29af481f9dc9db39c60d8f7c33d4350e189a6
Ethan Miller <siporlicecur@gmx.net>
2023-10-26 16:56:24 -0400

    refactor: Improve dashboard layout and responsiveness

--- src/visualizations/dashboard_components.py
+++ src/visualizations/dashboard_components.py
@@ -133,3 +133,5 @@ def create_individual_explanation_waterfall_chart(explanation_values, title="Ind
 # Change on 2023-10-12 10:01:52: chore: Upgrade Dash and Plotly dependencies
 
 # Change on 2023-10-23 10:39:56: fix: Correct data loading and preprocessing for XAI models
+
+# Change on 2023-10-26 16:56:24: refactor: Improve dashboard layout and responsiveness
