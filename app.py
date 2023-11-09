e1b9f5d4a48eb0a1c5ceb1543cf5607ac86b1f74
Ethan Miller <siporlicecur@gmx.net>
2023-11-09 14:00:40 -0500

    fix: Correct data loading and preprocessing for XAI models

--- app.py
+++ app.py
@@ -216,3 +216,5 @@ if __name__ == '__main__':
 # Change on 2023-10-27 16:49:54: fix: Correct data loading and preprocessing for XAI models
 
 # Change on 2023-11-06 16:38:31: refactor: Improve dashboard layout and responsiveness
+
+# Change on 2023-11-09 14:00:40: fix: Correct data loading and preprocessing for XAI models
