beb38f114abcc79581e18f8915eb247922272e40
Ethan Miller <siporlicecur@gmx.net>
2023-12-11 11:48:23 -0500

    chore: Upgrade Dash and Plotly dependencies

--- src/visualizations/dashboard_components.py
+++ src/visualizations/dashboard_components.py
@@ -161,3 +161,5 @@ def create_individual_explanation_waterfall_chart(explanation_values, title="Ind
 # Change on 2023-12-06 12:21:09: feat: Add support for new XAI methods (e.g., Grad-CAM)
 
 # Change on 2023-12-11 11:30:28: feat: Integrate LIME and SHAP explainers into the dashboard
+
+# Change on 2023-12-11 11:48:23: chore: Upgrade Dash and Plotly dependencies
