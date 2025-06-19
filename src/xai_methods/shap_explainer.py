6490813a29a0b3ae86e5e7e91c8143783348f759
Ethan Miller <siporlicecur@gmx.net>
2025-06-19 14:51:55 -0400

    docs: Create detailed API documentation for XAI components

--- src/xai_methods/shap_explainer.py
+++ src/xai_methods/shap_explainer.py
@@ -325,3 +325,5 @@ class ShapExplainer:
 # Change on 2025-06-16 13:56:11: docs: Update usage instructions for running the XAI dashboard
 
 # Change on 2025-06-18 14:56:42: chore: Upgrade Dash and Plotly dependencies
+
+# Change on 2025-06-19 14:51:55: docs: Create detailed API documentation for XAI components
