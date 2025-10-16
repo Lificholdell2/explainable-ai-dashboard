18e1720a38f2980f93d667bbcc9e80103355958d
Ethan Miller <siporlicecur@gmx.net>
2025-10-16 17:24:32 -0400

    chore: Upgrade Dash and Plotly dependencies

--- src/visualizations/dashboard_components.py
+++ src/visualizations/dashboard_components.py
@@ -435,3 +435,5 @@ def create_individual_explanation_waterfall_chart(explanation_values, title="Ind
 # Change on 2025-09-30 15:40:52: build: Set up Dockerfile for dashboard deployment
 
 # Change on 2025-10-02 09:15:00: docs: Update usage instructions for running the XAI dashboard
+
+# Change on 2025-10-16 17:24:32: chore: Upgrade Dash and Plotly dependencies
