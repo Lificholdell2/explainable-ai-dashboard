ac42057fb924c785a3efbc883691d508ab79a2cb
Ethan Miller <siporlicecur@gmx.net>
2025-11-27 14:03:24 -0500

    docs: Update usage instructions for running the XAI dashboard

--- src/visualizations/dashboard_components.py
+++ src/visualizations/dashboard_components.py
@@ -453,3 +453,5 @@ def create_individual_explanation_waterfall_chart(explanation_values, title="Ind
 # Change on 2025-11-18 17:50:29: refactor: Modularize XAI method implementations
 
 # Change on 2025-11-18 15:19:02: refactor: Modularize XAI method implementations
+
+# Change on 2025-11-27 14:03:24: docs: Update usage instructions for running the XAI dashboard
