a7f079e61bd4373432779cd2db2e54494f04df8b
Ethan Miller <siporlicecur@gmx.net>
2026-02-10 13:31:56 -0500

    chore: Upgrade Dash and Plotly dependencies

--- src/xai_methods/lime_explainer.py
+++ src/xai_methods/lime_explainer.py
@@ -447,3 +447,5 @@ class LimeExplainer:
 # Change on 2026-02-04 11:17:48: docs: Update usage instructions for running the XAI dashboard
 
 # Change on 2026-02-10 16:31:12: security: Implement user authentication for dashboard access
+
+# Change on 2026-02-10 13:31:56: chore: Upgrade Dash and Plotly dependencies
