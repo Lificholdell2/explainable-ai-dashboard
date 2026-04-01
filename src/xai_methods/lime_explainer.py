d04167337d26faa83da1fddbc9c9211740eb8403
Ethan Miller <siporlicecur@gmx.net>
2026-04-01 16:19:22 -0400

    test: Add unit tests for XAI explanation generation

--- src/xai_methods/lime_explainer.py
+++ src/xai_methods/lime_explainer.py
@@ -473,3 +473,5 @@ class LimeExplainer:
 # Change on 2026-03-27 12:15:47: feat: Add support for new XAI methods (e.g., Grad-CAM)
 
 # Change on 2026-04-01 09:23:18: docs: Create detailed API documentation for XAI components
+
+# Change on 2026-04-01 16:19:22: test: Add unit tests for XAI explanation generation
